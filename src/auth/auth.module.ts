959667efd30b4191a4958e3dc17f7bba28e4a3f2
Rodrigo Sandor <rod.sandor@gmail.com>
2022-06-09 19:09:31 -0300

    feat(auth): implement login, guard and localStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
+import { PassportModule } from '@nestjs/passport';
 import { Module } from '@nestjs/common';
+
 import { AuthController } from './auth.controller';
+import { UserModule } from './../user/user.module';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '30d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
