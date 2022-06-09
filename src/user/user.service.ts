e19b4151f9626401e65220c2c1edaf08775b31af
Rodrigo Sandor <rod.sandor@gmail.com>
2022-06-09 18:31:50 -0300

    added project

---
+++ src/user/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+
+import * as bcrypt from 'bcrypt';
+
+import { PrismaService } from './../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const data = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+
+    const createdUser = await this.prisma.user.create({ data });
+    return {
+      ...createdUser,
+      password: undefined,
+    };
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
